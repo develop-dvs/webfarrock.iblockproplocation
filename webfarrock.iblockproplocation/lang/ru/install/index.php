9d6f67b6025d3924ff80fdb410e4f7a3a2f4ae30
Develop <develop@divasoft.ru>
2022-01-21 20:09:13 +0300

    Update index.php

--- webfarrock.iblockproplocation/lang/ru/install/index.php
+++ webfarrock.iblockproplocation/lang/ru/install/index.php
@@ -1,5 +1,5 @@
 <?php
-$MESS["WEBFARROCK_IBLOCKPROP_LOCATION_MODULE_NAME"] = "Свойство инфоблока - привязка к местоположению 2.0";
-$MESS["WEBFARROCK_IBLOCKPROP_LOCATION_MODULE_DESCRIPTION"] = "Добавляет новое свойство инфоблока - \"привязка к местоположению 2.0 с автозаполнением\"";
+$MESS["WEBFARROCK_IBLOCKPROP_LOCATION_MODULE_NAME"] = "Свойство - привязка к местоположению 2.0";
+$MESS["WEBFARROCK_IBLOCKPROP_LOCATION_MODULE_DESCRIPTION"] = "Добавляет новое свойство - \"привязка к местоположению 2.0 с автозаполнением\"";
 $MESS["WEBFARROCK_IBLOCKPROP_LOCATION_INSTALL"] = "Этот модуль требует наличия установленного модуля \"Интернет-магазин\"";
 $MESS["WEBFARROCK_IBLOCKPROP_LOCATION_INSTALL_TITLE"] = "Установка модуля \"Свойство инфоблока - привязка к местоположению 2.0\"";
